709e30f75f11e8b2db7e6b09646554e1f692b112
HBW-Dev <w17371322851@gmail.com>
2025-12-06 02:21:49 +0000

    feat(study_note.py) and CRUD operations for List and Dict

--- study_notes.py
+++ study_notes.py
@@ -5,10 +5,34 @@
 players = ['zhang san', 'li si', 'wang wu', 'zhao liq', 'zhou qi']
 print(players[0:3])
 
+players.append('xi ba') # Add
+print(players)
+
+del players[1]  # Delete (Remove by index)
+print(players)
+
+players.remove('wang wu') # Delete (Remove by value)
+print(players)
+
+players[0] = 'zhang ling' # Update
+print(players[0])
+
+# Search: Check if exists
+player = "wang wu"
+if player in players:
+    print(player)
+else:
+    print("Not found")
+
+# Search: Find position
+location = players.index('zhang ling')
+print(f"zhang ling is at index {location}")
 # Task: Print the last 3 players using negative slicing
 print(players[-3:])
 
+# Copy
 new_players = players[:]
+print(new_players)
 
 # --- 2. Dictionary.get() ---
 # Define a dictionary for a user (without 'age' key)
@@ -22,6 +46,21 @@ print(user['id'])
 # print(user['age']) error
 print(user.get('age'))
 
+user['age'] = 26 # Add
+print(user.get('age'))
+
+del user['name'] # Delete
+print(user.get('name'))
+
+print(user.pop('id')) # Delete and return value
+print(user.get('id'))
+
+print(user.get('id', '01')) 
+
+if '01' in user:
+    print(f'{user.get('name')} has an ID')
+else: 
+    print(f'{user.get('name')} has no ID')
 # --- 3. f-string Formatting ---
 # Dfine a float variable pi = 3.1415926
 pi = 3.1415926
